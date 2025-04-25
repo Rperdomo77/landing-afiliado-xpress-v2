f9bed96255b9a9689162f25b71bd0957ffd1a3a6
Rperdomo77 <raulperdomo@hotmail.com>
2025-04-25 01:07:14 -0400

    Add files via upload
    listo

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import LandingAfiliadoXpress from './pages/LandingAfiliadoXpress';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <LandingAfiliadoXpress />
+  </React.StrictMode>
+);
